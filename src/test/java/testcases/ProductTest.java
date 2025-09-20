d3d9d24156564acce3b87455ab921939e41e86ca
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-09-20 22:46:03 +0530

    added getSingleProduct test

--- src/test/java/testcases/ProductTest.java
+++ src/test/java/testcases/ProductTest.java
@@ -13,7 +13,24 @@ public class ProductTest extends BaseClass {
 	// test to retrieve all products
 	@Test
 	public void testGetAllProducts() {
-		given().when().get(Endpoints.GET_ALL_PRODUCTS).then().statusCode(200).body("size()", greaterThan(0)).log().body();
+		given().when().get(Endpoints.GET_ALL_PRODUCTS).then().statusCode(200).body("size()", greaterThan(0)).log()
+				.body();
+	}
+	
+	
+	// test to retrieve single product by ID
+	@Test
+	public void getSingleProduct() {
+		
+		int productId = configReader.getIntProperty("productID");
+		
+		given()
+			.pathParam("id", productId)
+		.when()
+			.get(Endpoints.GET_PRODUCT_BY_ID)
+		.then()
+			.statusCode(200)
+			.log().body();
 	}
 
 }
