b63c09dd02acafac29c43b5fd2a5a8c628787bb0
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-09-28 19:54:46 +0530

    Added tests for get single user by ID and create new user

--- src/test/java/testcases/UsersTest.java
+++ src/test/java/testcases/UsersTest.java
@@ -4,6 +4,9 @@ import org.testng.annotations.Test;
 
 import endpoint.Endpoints;
 import io.restassured.http.ContentType;
+import payloads.Payload;
+import pojo.Products;
+import pojo.Users;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,6 +31,43 @@ public class UsersTest extends BaseClass {
 				.body("size()", greaterThan(0))
 				.log().body();
 
+	}
+
+	// Get user by ID
+	@Test
+	public void testGetUserByID() {
+
+		int userId = configReader.getIntProperty("userId");
+
+		given()
+				.pathParam("id", userId)
+				.when()
+				.get(Endpoints.GET_USER_BY_ID)
+				.then()
+				.statusCode(200)
+				.contentType(ContentType.JSON)
+				.assertThat().body(matchesJsonSchemaInClasspath("schemas/singleUserSchema.json"))
+				.body("size()", greaterThan(0))
+				.log().body();
+	}
+
+	// Create new User
+	@Test
+	public void testCreateUser() {
+
+		int userID;
+		Users user = Payload.usersPayload();
+
+		userID = given()
+				.contentType(ContentType.JSON)
+				.body(user)
+				.when()
+				.post(Endpoints.CREATE_USER)
+				.then()
+				.log().body()
+				.statusCode(201)
+				.body("id", notNullValue())
+				.extract().response().jsonPath().getInt("id");
 	}
 
 }
